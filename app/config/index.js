f2e195d8c7a37480ce2a4a72fb6a4fd5d05a894a
enaime hamza <hamza.enaime@outlook.com>
2019-02-05 21:41:14 +0100

    fix config

--- app/config/index.js
+++ app/config/index.js
@@ -1,14 +1,14 @@
 "use strict";
 let connectionStrings = {
   production: process.env.CONNECTION_STRING,
-  development: "mongodb://localhost/events-db"
+  development:
+    "mongodb://thisevent:thisevent123456@ds245277.mlab.com:45277/thisevent"
 };
 
 // TODO: SET SESSION_SECRET AND CONNECTION_STRING CONSTANTS
 module.exports = {
   environment: process.env.NODE_ENV || "development",
-  connectionString:
-    "mongodb://thisevent:thisevent123456@ds245277.mlab.com:45277/thisevent",
+  connectionString: connectionStrings[process.env.NODE_ENV || "development"],
   port: process.env.PORT || 3003,
   sessionSecret: process.env.SESSION_SECRET || "[session_secret]"
 };
