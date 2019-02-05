7609a44a97884f54496cac3732650159ff82756f
enaime hamza <hamza.enaime@outlook.com>
2019-02-05 21:58:36 +0100

    fix port env

--- app/config/application.js
+++ app/config/application.js
@@ -1,35 +1,39 @@
-'use strict';
+"use strict";
 
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    cookieParser = require('cookie-parser'),
-    passport = require('passport'),
-    path = require('path');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  session = require("express-session"),
+  cookieParser = require("cookie-parser"),
+  passport = require("passport"),
+  path = require("path");
 
 /* Setup App */
-module.exports = function(config){
-    let app = express();
+module.exports = function(config) {
+  let app = express();
 
-    app.set('view engine', 'pug');
-    app.set('views', path.join(__dirname, '../views'));
-    app.use('/static', express.static(path.join(__dirname, '../../public')));
+  app.set("view engine", "pug");
+  app.set("views", path.join(__dirname, "../views"));
+  app.use("/static", express.static(path.join(__dirname, "../../public")));
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(cookieParser());
-    app.use(session({
-        secret: config.sessionSecret,
-        resave: true,
-        saveUninitialized: true
-    }));
-    app.use(passport.initialize());
-    app.use(passport.session());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: config.sessionSecret,
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-    app.start = function(){
-        const port = config.port;
-        app.listen(port, () => console.log(`App running at: http://localhost:${port}`));
-    };
+  app.start = function() {
+    const port = process.env.PORT || config.port;
+    app.listen(port, () =>
+      console.log(`App running at: http://localhost:${port}`)
+    );
+  };
 
-    return app;
+  return app;
 };
