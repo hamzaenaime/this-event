a035d5b2e34ee3b959fbd2a8e31b1c4d4e90c871
enaime hamza <hamza.enaime@outlook.com>
2019-02-07 11:50:41 +0100

    remove logo

--- app/app.js
+++ app/app.js
@@ -9,3 +9,12 @@ require("./auth")(app, data);
 require("./routers")(app, data);
 
 app.start();
+
+/*
+            #logo.col-sm-2
+                a(href="/")
+                    span.fa-stack
+                        i.fa.logo-hex.fa-stack-2x
+                        i.fa.logo-fa.fa-map-marker.fa-stack-2x
+
+*/
