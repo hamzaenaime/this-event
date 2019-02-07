57132ad8bb756b53d848c5caff43122cb9edab65
ayoubhamaoui <ayoub10hamaoui@gmaiL.com>
2019-02-07 11:28:50 +0100

    fix mongoose error

--- app/config/database.js
+++ app/config/database.js
@@ -4,5 +4,8 @@ const mongoose = require("mongoose");
 
 module.exports = function(connectionString) {
   mongoose.Promise = global.Promise;
-  mongoose.connect(connectionString, { useNewUrlParser: true });
+  mongoose.connect("mongodb://thisevent:thisevent123456@ds245277.mlab.com:45277/thisevent", {
+  useMongoClient: true,
+  /* other options */
+});
 };
